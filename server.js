c82dc60b2536684e445e93fe0d9a6e51797b5501
Evg <Stan@google.com>
2021-07-28 16:56:35 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const auth = require('./auth.js');
+const express = require('express');
+const { connectToDb } = require('./db.js');
+const { installHandler } = require('./api_handler.js');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(cookieParser());
+app.use('/auth', auth.routes);
+installHandler(app);
+
+const port = process.env.API_SERVER_PORT || 3000;
+
+(async function () {
+  try {
+    await connectToDb();
+    app.listen(port, function () {
+      console.log(`API server started on port ${port}`);
+    });
+  } catch (err) {
+    console.log('ERROR:', err);
+} }());
